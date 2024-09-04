47d00ae2c078cc0658f35b3083b228c61b335109
Andre Hoffmann <3483518+effzehn@users.noreply.github.com>
2024-09-04 17:40:31 +0200

    Add platforms (untested)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Registry",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
     products: [
         .library(
             name: "Registry",
